b3a6693ad788f60434ed83b1aab3518ad3d8b0e2
Kenneth Wilcox <37879461+Drkjr92@users.noreply.github.com>
2019-01-16 15:17:34 -0500

    Add files via upload
    Quick start during lecture :p

---
+++ Main.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import java.util.Scanner;
+
+/*
+
+***************************************** Client Program **********************************************************
+
+IP address and port number are hard coded. Provided by e-mail. Group number 3.
+Machine numbers: 104 and 105
+104- Client Machine
+105- Server Machine
+IP address :
+Port number:
+
+Team Members:
+    Kenneth Wilcox
+    -
+    -
+    -
+    -
+    -
+    
+
+
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String ipAddress = "";
+        int portNumber   = 22;
+
+
+        System.out.print("Connecting to Server by port number..." + portNumber );
+        /*
+
+        block of code where the connection is going to be established.
+
+
+
+
+
+         */
+
+        System.out.println("Connection established. SUCCESS!");
+
+        /*
+
+        block of code where we will present the user options and key words to type.
+
+
+
+
+
+         */
+
+
+        Scanner userInput = new Scanner(System.in);
+
+        String userQuit = "Quit";
+
+        while(userInput != userInput)
+        {
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
