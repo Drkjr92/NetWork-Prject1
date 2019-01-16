79947690ba5cd18a110debc733d33dcfff349a8b
Kenneth Wilcox <37879461+Drkjr92@users.noreply.github.com>
2019-01-16 15:45:33 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -51,8 +51,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String ipAddress = "";
-        int portNumber   = 22;
 
 
         System.out.print("Connecting to Server by port number..." + portNumber );
