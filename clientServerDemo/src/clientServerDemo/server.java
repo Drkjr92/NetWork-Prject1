fc05d37f4d1b362c54bf31d255eb0e3a5deafdd6
kaliefl <kalief_leecan@yahoo.com>
2019-04-04 20:46:33 -0400

    Update server.java

--- clientServerDemo/src/clientServerDemo/server.java
+++ clientServerDemo/src/clientServerDemo/server.java
@@ -1,4 +1,4 @@
-package clientServerDemo;
+package ServerClient;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -39,126 +39,14 @@ public class server {
 		
 		//Sets number to user input for switch
 		number = input.nextInt();
+		
+		ServerThread sThread = new ServerThread(number, serverSocket, ss);
+		sThread.run();
 			
-		//Get request based on user input
-		switch (number) {
-			
-		//Date & Time Request
-			case 1:
-				System.out.println("Request:  Current Date and Time");
-				Date date = new Date();
-				
-				temp = "Current Date & Time: " + date.toString();
-				
-				System.out.println("Sent\n");
-				break;
-
-				//Uptime Request
-			case 2:
-				System.out.println("Request:  Uptime");
-				Process uptime = Runtime.getRuntime().exec("uptime");
-				
-				brRead = new BufferedReader (new 
-						InputStreamReader(uptime.getInputStream()));
-				
-				temp = brRead.readLine();
-				
-				System.out.println("Sent\n");
-				break;
-				
-				//Memory Usage Request
-			case 3:
-				System.out.println("Request:  Memory Use");
-				
-				long totalMemory = Runtime.getRuntime().totalMemory();
-				long freeMemory = Runtime.getRuntime().freeMemory();;
-				long usedMemory = totalMemory - freeMemory;
-				
-				temp = ("Current Total Memory: " + totalMemory + 
-						"\nCurrent Free Memory:  " + freeMemory +
-						"\nCurrent Memory Usage:   " + usedMemory);
-				System.out.println("Sent\n");
-				break;
-				
-				//Netstat Request
-			case 4:
-				System.out.println("Request:  Netstat");
-				
-				Process netstat = Runtime.getRuntime().exec("netstat");
-				
-				brRead = new BufferedReader (new 
-						InputStreamReader(netstat.getInputStream()));
-				brCount = new BufferedReader (new 
-						InputStreamReader(netstat.getInputStream()));
-				
-				
-				temp = brRead.readLine();
-				lineCount = brCount.lines().count();
-				//Trying to add lines to "temp"
-				
-				System.out.println("Lines counted: " + lineCount);
-				int i;
-				for(i = 0; i < lineCount - 1; i++)
-					temp = temp + brRead.readLine();
-				
-				System.out.println("Sent\n");
-				break;
-				
-				//Users Request
-			case 5:
-				System.out.println("Request: Users");
-				
-				Process users = Runtime.getRuntime().exec("users");
-				
-				brRead = new BufferedReader (new 
-						InputStreamReader(users.getInputStream()));
-				
-				temp = brRead.readLine();
-				
-				System.out.println("Sent\n");
-				break;
-				
-				//Running Processes Request
-			case 6:
-				System.out.println("Request:  Running Processes");
-				
-				Process ps = Runtime.getRuntime().exec("ps");
-				
-				brRead = new BufferedReader (new 
-						InputStreamReader(ps.getInputStream()));
-				brCount = new BufferedReader (new 
-						InputStreamReader(ps.getInputStream()));
-				
-				
-				temp = brRead.readLine();
-				lineCount = brCount.lines().count();
-				//Trying to add lines to "temp"
-				
-				System.out.println("Lines counted: " + lineCount);
-				int j;
-				for(j = 0; j < lineCount - 1; j++)
-					temp = temp + brRead.readLine();
-				
-				System.out.println("Sent\n");
-				break;
-				
-				//Exit Case
-			case 7:
-				System.exit(0);
-				break;
-				
-				//Invalid Request Case
-			default:
-				System.out.println("Invalid Request\nNo Response Sent\n");
-				break;
-			}//end switch
-			
-			//Sends "temp" string to client
-			PrintStream ps = new PrintStream(ss.getOutputStream());
-			ps.println(temp);
-			
-			temp = "";
-			}//end while
+		PrintStream ps = new PrintStream(ss.getOutputStream());
+		ps.println(temp);
+		
+				}//end while
 			
 			//Closes server
 			//System.out.println("Request: Terminate Connection");
@@ -169,3 +57,166 @@ public class server {
 
 }//end server
 
+class ServerThread implements Runnable
+{
+
+	int number;
+	ServerSocket serverSocket;
+	Socket ss;
+
+    public ServerThread(int number, ServerSocket serverSocket, Socket ss)
+    {
+        this.number = number;
+        this.serverSocket = serverSocket;
+        this.ss = ss;
+
+    }//end ServerThread
+
+    //override method to connect to server
+    //clienttoserver connection
+    @Override
+    public void run()
+    {
+    	
+    	runServerThread(number, serverSocket, ss);
+
+    }//end run
+
+
+    public static String runServerThread(int number, ServerSocket serverSocket, Socket ss)
+    {
+    	String temp = "";//String used to output info from server
+		long lineCount;//Long used to get # of lines from BufferedReader
+		BufferedReader brRead, brCount;
+		
+		try{
+    	
+    	//Get request based on user input
+    			switch (number) {
+    				
+    			//Date & Time Request
+    				case 1:
+    					System.out.println("Request:  Current Date and Time");
+    					Date date = new Date();
+    					
+    					temp = "Current Date & Time: " + date.toString();
+    					
+    					System.out.println("Sent\n");
+    					break;
+
+    					//Uptime Request
+    				case 2:
+    					System.out.println("Request:  Uptime");
+    					Process uptime = Runtime.getRuntime().exec("uptime");
+    					
+    					brRead = new BufferedReader (new 
+    							InputStreamReader(uptime.getInputStream()));
+    					
+    					temp = brRead.readLine();
+    					
+    					System.out.println("Sent\n");
+    					break;
+    					
+    					//Memory Usage Request
+    				case 3:
+    					System.out.println("Request:  Memory Use");
+    					
+    					long totalMemory = Runtime.getRuntime().totalMemory();
+    					long freeMemory = Runtime.getRuntime().freeMemory();;
+    					long usedMemory = totalMemory - freeMemory;
+    					
+    					temp = ("Current Total Memory: " + totalMemory + 
+    							"\nCurrent Free Memory:  " + freeMemory +
+    							"\nCurrent Memory Usage:   " + usedMemory);
+    					System.out.println("Sent\n");
+    					break;
+    					
+    					//Netstat Request
+    				case 4:
+    					System.out.println("Request:  Netstat");
+    					
+    					Process netstat = Runtime.getRuntime().exec("netstat");
+    					
+    					brRead = new BufferedReader (new 
+    							InputStreamReader(netstat.getInputStream()));
+    					brCount = new BufferedReader (new 
+    							InputStreamReader(netstat.getInputStream()));
+    					
+    					
+    					temp = brRead.readLine();
+    					lineCount = brCount.lines().count();
+    					//Trying to add lines to "temp"
+    					
+    					System.out.println("Lines counted: " + lineCount);
+    					int i;
+    					for(i = 0; i < lineCount - 1; i++)
+    						temp = temp + brRead.readLine();
+    					
+    					System.out.println("Sent\n");
+    					break;
+    					
+    					//Users Request
+    				case 5:
+    					System.out.println("Request: Users");
+    					
+    					Process users = Runtime.getRuntime().exec("users");
+    					
+    					brRead = new BufferedReader (new 
+    							InputStreamReader(users.getInputStream()));
+    					
+    					temp = brRead.readLine();
+    					
+    					System.out.println("Sent\n");
+    					break;
+    					
+    					//Running Processes Request
+    				case 6:
+    					System.out.println("Request:  Running Processes");
+    					
+    					Process ps = Runtime.getRuntime().exec("ps -e");
+    					
+    					brRead = new BufferedReader (new 
+    							InputStreamReader(ps.getInputStream()));
+    					brCount = new BufferedReader (new 
+    							InputStreamReader(ps.getInputStream()));
+    					
+    					
+    					temp = brRead.readLine();
+    					lineCount = brCount.lines().count();
+    					//Trying to add lines to "temp"
+    					
+    					System.out.println("Lines counted: " + lineCount);
+    					int j;
+    					for(j = 0; j < lineCount - 1; j++)
+    						temp = temp + brRead.readLine();
+    					
+    					System.out.println("Sent\n");
+    					break;
+    					
+    					//Exit Case
+    				case 7:
+    					System.exit(0);
+    					break;
+    					
+    					//Invalid Request Case
+    				default:
+    					System.out.println("Invalid Request\nNo Response Sent\n");
+    					break;
+    				}//end switch
+    				
+    				///Sends "temp" string to client
+    				PrintStream ps = new PrintStream(ss.getOutputStream());
+    				ps.println(temp);
+    				
+    				///temp = "";
+    				
+    }//end try
+		
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+		return temp;
+
+    }//end runServerThread
+
+}//end class ServerThread implements Runnable
